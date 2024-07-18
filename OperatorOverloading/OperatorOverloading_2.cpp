572c4be13d79b28e912110c9958ef10bee4ec7ec
GiannisKarampinis <gikamc@yahoo.gr>
2024-07-18 15:16:58 +0300

    Add operator overloading with friend function example

---
+++ OperatorOverloading/OperatorOverloading_2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    public:
+        int real;
+        int img;
+
+        Complex add(Complex c)
+        {
+            Complex temp;
+
+            temp.real = real + c.real;
+            temp.img = img + c.img;
+
+            return temp;
+        }
+
+        friend Complex operator+(Complex c1, Complex c2);
+        
+};
+
+Complex operator+(Complex c1, Complex c2)
+{
+    Complex temp;
+
+    temp.real = c1.real + c2.real;
+    temp.img = c1.img + c2.img;
+
+    return temp;
+}
+
+int main()
+{
+    Complex c1, c2, c3, c4;
+
+    c1.real = 5;
+    c1.img = 3;
+
+    c2.real = 10;
+    c2.img = 5;
+
+    c3 = c1.add(c2);
+    c4 = c1 + c2;
+
+    cout << c3.real << " +i" << c3.img << endl;
+    cout << c4.real << " +i" << c4.img << endl;
+
+    return 0;
+}
